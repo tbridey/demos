e4fde186ebe8ea08e69066745d0b89ebe551eee7
unknown <moberlies1337@gmail.com>
2020-09-17 09:30:49 -0700

    Improved SpringMVCDemo to use ResponseEntity for custom HTTP Response Status Codes

--- week6/SpringMVCDemo/src/main/java/com/revature/controllers/PizzaController.java
+++ week6/SpringMVCDemo/src/main/java/com/revature/controllers/PizzaController.java
@@ -3,6 +3,8 @@ package com.revature.controllers;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,29 +23,44 @@ public class PizzaController {
 
 	@GetMapping(path = "pizza", produces = "application/json")
 	@ResponseBody
-	public Set<Pizza> findAll() {
-		return service.findAll();
+	public ResponseEntity< Set<Pizza> > findAll() {
+		Set<Pizza> result = service.findAll();
+		
+		if(result.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.ok(result);
 	}
 	
 	@GetMapping("pizza/{id}")
 	@ResponseBody
-	public Pizza findById(@PathVariable("id") int id) {
+	public ResponseEntity<Pizza> findById(@PathVariable("id") int id) {
 		Set<Pizza> all = service.findAll();
 		
 		for(Pizza p : all) {
 			if(p.getId() == id) {
-				return p;
+				return ResponseEntity.ok(p);
 			}
 		}
 		
-		return null;
+		return ResponseEntity.noContent().build();
 	}
 	
 	@PostMapping("pizza")
 	@ResponseBody
-	public Pizza insert(@RequestBody Pizza p) {
+	public ResponseEntity<Pizza> insert(@RequestBody Pizza p) {
+		if(p.getId() != 0) {
+			return ResponseEntity.badRequest().build();
+		}
+		
 		service.save(p);
 		
-		return p;
+		if(p.getId() == 0) {
+			// Failed to insert properly
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		
+		return ResponseEntity.accepted().body(p);
 	}
 }
