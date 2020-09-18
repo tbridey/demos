56b40a44aa6765588f39f1dd2d27337f2710e8dc
unknown <moberlies1337@gmail.com>
2020-09-18 07:57:41 -0700

    Add MovkMvc and RestAssuredMockMvc example tests

--- week6/SpringBootDemo/src/test/java/com/revature/SpringBootDemoApplicationTests.java
+++ week6/SpringBootDemo/src/test/java/com/revature/SpringBootDemoApplicationTests.java
@@ -1,13 +1,46 @@
 package com.revature;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.revature.controllers.HelloController;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
+	
+//	@Autowired
+//	private WebApplicationContext wac;
 
-	@Test
-	void contextLoads() {
+	private MockMvc mock;
+	
+	@BeforeEach
+	public void initBeforeTest() {
+		mock = MockMvcBuilders.standaloneSetup(new HelloController()).build(); // Used for Unit Tests
+//		mock = MockMvcBuilders.webAppContextSetup(wac).build(); // Used for Integration Tests
 	}
-
+	
+	@Test
+	void testHelloWorld() throws Exception {
+		mock.perform(get("/hello"))
+			.andDo(print())
+			.andExpect(status().isIAmATeapot())
+			.andExpect(jsonPath("$").value("Hello World! This is my very first Spring Boot Application!"));
+	}
+	
+	@Test
+	void testPostNotAllowed() throws Exception {
+		mock.perform(post("/hello"))
+		.andDo(print())
+		.andExpect(status().isMethodNotAllowed());
+	}
 }
--- week6/SpringDataDemo/src/test/java/com/revature/SpringDataDemoApplicationTests.java
+++ week6/SpringDataDemo/src/test/java/com/revature/SpringDataDemoApplicationTests.java
@@ -1,13 +1,55 @@
 package com.revature;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.revature.controllers.UserController;
+import com.revature.repositories.UserRepository;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 
 @SpringBootTest
 class SpringDataDemoApplicationTests {
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	@Mock
+	private UserRepository userDao;
+	
+	@InjectMocks
+	private UserController userController;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	void init() {
+//		RestAssuredMockMvc.standaloneSetup(userController); // Unit Test setup
+		RestAssuredMockMvc.webAppContextSetup(wac); // Integration Test setup
 	}
 
+	@Test
+	void testThatGivenNoUsersFindAllReturnsEmptyArrayAndOkStatus() {
+		when(userDao.findAll()).thenReturn(Collections.emptyList());
+		
+		given()
+			.when()
+				.get("/users")
+			.then()
+				.log().ifValidationFails()
+				.statusCode(200)
+				.contentType(ContentType.JSON)
+				.body(is(equalTo("[]")));
+	}
 }
